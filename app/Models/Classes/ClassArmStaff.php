37132c8316295a03ab1c3e2a01561be1df28f6c0
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-04-11 00:28:39 +0100

    create classArmStaff model

---
+++ app/Models/Classes/ClassArmStaff.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Classes;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClassArmStaff extends Model
+{
+    use HasFactory;
+}
