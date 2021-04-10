bd6c961122a219ff3ef605733bb96f01eb4aface
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-04-11 00:32:51 +0100

    Update ClassArmStaff.php

--- app/Models/Classes/ClassArmStaff.php
+++ app/Models/Classes/ClassArmStaff.php
@@ -2,10 +2,13 @@
 
 namespace App\Models\Classes;
 
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ClassArmStaff extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    protected $fillable = [];
 }
