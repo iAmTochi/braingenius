6c69da01acc555bc6aec4e50854578b66d9b8fa9
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-04-11 02:06:50 +0100

    Update ActivityLog.php

--- app/Models/Activity/ActivityLog.php
+++ app/Models/Activity/ActivityLog.php
@@ -2,9 +2,26 @@
 
 namespace App\Models\Activity;
 
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ActivityLog extends Model
 {
-    //
+    use SoftDeletes, HasFactory, Uuid;
+
+    protected $fillable = [
+        'user_uuid',
+        'activity',
+        'ip_address',
+        'platform',
+        'browser',
+    ];
+
+    public function create_activity($activity,$user_uuid, $ip_address, $platform, $browser){
+        $this->create([
+
+        ]);
+    }
 }
